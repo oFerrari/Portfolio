5fb5cb3f63ef91b81a7968a85b5d5a3e36a7552e
oFerrari <andrei.domingos@estudante.ifms.edu.br>
2024-03-07 19:35:53 -0400

    Iniciando Projeto Porfolio Novo

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-let nomeCabecalho = document.querySelector('.nome-cabecalho')
-
-nomeCabecalho.addEventListener('mouseover',() => {
-    setTimeout(() => {nomeCabecalho.innerText = 'Andrei Ferrari Domingos'},1000)
-})
-
-
